30caf130d5a9b2db265efb02914915a111923a0f
ivan montañes <ivantuckp12345@gmail.com>
2024-03-31 02:20:14 -0300

    punto 3)e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("hola mundo/n");
+    //declaramos una variable de tipo entero//
+    int n = 10;
+    //declaramos un puntero que apunta a una variable de tipo entero//
+    int *p_n = &n;
+    //mostramos el contenido del puntero//
+    printf("contenido del puntero: %p\n",p_n);
+    //mostramos la direccion de memoria almacenada en el puntero//
+    printf("direccion de memoria almacenada en el puntero: %p\n",p_n);
+    //mostramos la direccion de memoria de la variable//
+    printf("direccion de memoria de la variable: %p\n",&n);
+    //mostramos la direccion de memoria del puntero//
+    printf("direccion de memoria del puntero: %p\n",&p_n);
+    //mostramos el tamaño de memoria utilizado por la variable//
+    printf("tamaño de memoria utilizado por la variable: %d\n",sizeof(n));
     return 0;
 }
